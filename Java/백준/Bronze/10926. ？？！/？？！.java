a5070901e79c07fcfe19a4e363d3421d804893b1
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-04-02 17:27:55 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    //main
+    public static void main(String[] args) throws IOException {
+        System.out.println(sb.append(br.readLine()).append("??!"));
+    }
+}
