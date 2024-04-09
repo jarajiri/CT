f0478311bc3a7f8661ee54b9671d8a306619726b
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-04-09 15:11:15 +0900

    [Silver IV] Title: 주몽, Time: 408 ms, Memory: 17968 KB -BaekjoonHub

---
+++ Java/백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,27 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j]==M) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
