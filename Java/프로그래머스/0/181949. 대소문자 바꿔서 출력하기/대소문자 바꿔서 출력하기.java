8a9c90a145d0fe28661d98a81a1931c57cfe5293
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-09-10 17:15:15 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 186.57 ms, Memory: 64.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ Java/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -4,17 +4,16 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        char c;
-        for (int i = 0; i < str.length(); i++) {
-            c = str.charAt(i);
-            if(c >= 64 && c <= 90){// 대문자
-                c += 32;
-                System.out.print(c);
-            }
-            else{
-                c -=32;
-                System.out.print(c);
+
+        StringBuilder answer = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                answer.append(Character.toUpperCase(c));
+            } else {
+                answer.append(Character.toLowerCase(c));
             }
         }
+        System.out.println(answer);
+        sc.close();
     }
 }
