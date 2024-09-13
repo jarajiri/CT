767570048a66c5b730e213d84759f97866845048
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-09-13 15:15:28 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.47 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int log = numLog[i + 1] - numLog[i];
+            switch (log) {
+                case 1:
+                    sb.append("w");
+                    break;
+                case 10:
+                    sb.append("d");
+                    break;
+                case -1:
+                    sb.append("s");
+                    break;
+                case -10:
+                    sb.append("a");
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
