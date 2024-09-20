6299edd6cd7d54a7a9f9ef8ae66d49a7d3c8a98d
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-09-20 15:25:01 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 11.08 ms, Memory: 83.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,14 +1,20 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
         StringBuilder SB = new StringBuilder(my_string);
-        StringBuilder reverseSB = new StringBuilder();
+
         for (int[] query : queries) {
-            String left = SB.substring(0, query[0]);
-            String center = SB.substring(query[0], query[1] + 1);
-            String right = SB.substring(query[1] + 1);
-            center = reverseSB.append(center).reverse().toString();
-            SB.replace(0,my_string.length(),left+center+right);
-            reverseSB.replace(0,reverseSB.length(),"");
+            int start = query[0];
+            int end = query[1];
+
+            // 쿼리 범위 내 문자열 뒤집기
+            while (start < end) {
+                // 두 값을 교환
+                char temp = SB.charAt(start);
+                SB.setCharAt(start, SB.charAt(end));
+                SB.setCharAt(end, temp);
+                start++;
+                end--;
+            }
         }
 
         return SB.toString();
