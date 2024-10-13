401a9822e9ef0c9ddd796f398ae7166622fa0691
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-10-13 16:46:15 +0900

    [level 0] Title: k의 개수, Time: 3.72 ms, Memory: 74.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120887. k의 개수/k의 개수.java
+++ Java/프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int count = 0;
-        String strK = String.valueOf(k);
-        for (int l = i; l <= j; l++) {
-            String value = String.valueOf(l);
-            if (value.contains(strK)) {
-                String[] array = value.split("");
-                for (String alpha : array) {
-                    if (alpha.equals(strK)) count++;
+        int answer = 0;
+        for(int z = i; z<=j; z++) {
+            int x = z;
+            while (x != 0) {
+                if (x % 10 == k) {
+                    answer++;
                 }
+                x /= 10;
             }
         }
-        return count;
+
+        return answer;
     }
 }
