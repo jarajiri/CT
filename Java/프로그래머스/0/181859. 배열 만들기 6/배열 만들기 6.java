6f168d479d7659afc9c45293d7add307c2fc1b35
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-10-14 18:19:51 +0900

    [level 0] Title: 배열 만들기 6, Time: 32.65 ms, Memory: 138 MB -BaekjoonHub

--- Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,15 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> stk = new ArrayList<>();
-        for (int j : arr) {
-            if (stk.isEmpty()) stk.add(j);
-            else if (stk.get(stk.size() - 1).equals(j)) stk.remove(stk.size() - 1);
-            else stk.add(j);
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int i : arr) {
+            if (!stk.isEmpty() && stk.peekLast() == i) {
+                stk.removeLast();
+            } else {
+                stk.addLast(i);
+            }
         }
-        if(stk.isEmpty()) return new int[]{-1};
-        return stk.stream().mapToInt(Integer::intValue).toArray();
+        return stk.isEmpty() ? new int[]{-1} : stk.stream().mapToInt(Integer::intValue).toArray();
     }
 }
