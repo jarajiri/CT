e9bafefa6da304dea05ae73dd8cd9f341002daf6
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2024-12-10 15:48:09 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 80.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ Java/프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,12 +1,11 @@
 class Solution {
     public static String solution(int age) {
-        String[] strs = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         while (age > 0) {
             int digit = age % 10;
-            answer = strs[digit] + answer;
+            sb.insert(0, (char) (digit + 97));
             age /= 10;
         }
-        return answer;
+        return sb.toString();
     }
 }
