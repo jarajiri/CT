71feabfb8fadf0248725b1a10ff14f0d12e07a09
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2024-12-11 17:37:21 +0900

    [level 0] Title: 모스부호 (1), Time: 20.38 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ Java/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,39 +1,15 @@
-import java.util.*;
-
 class Solution {
     public String solution(String letter) {
-        StringBuilder sb = new StringBuilder();
-        Map<String,String> morse = new HashMap<>();
-        morse.put(".-", "a");
-        morse.put("-...", "b");
-        morse.put("-.-.", "c");
-        morse.put("-..", "d");
-        morse.put(".", "e");
-        morse.put("..-.", "f");
-        morse.put("--.", "g");
-        morse.put("....", "h");
-        morse.put("..", "i");
-        morse.put(".---", "j");
-        morse.put("-.-", "k");
-        morse.put(".-..", "l");
-        morse.put("--", "m");
-        morse.put("-.", "n");
-        morse.put("---", "o");
-        morse.put(".--.", "p");
-        morse.put("--.-", "q");
-        morse.put(".-.", "r");
-        morse.put("...", "s");
-        morse.put("-", "t");
-        morse.put("..-", "u");
-        morse.put("...-", "v");
-        morse.put(".--", "w");
-        morse.put("-..-", "x");
-        morse.put("-.--", "y");
-        morse.put("--..", "z");
-        String[] split = letter.split(" ");
-        for (String s : split) {
-            sb.append(morse.get(s));
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] word = letter.split(" ");
+
+        for(int i=0;i<word.length;i++){
+            for(int j=0;j<morse.length;j++){
+                if(word[i].equals(morse[j])) answer+=(char)(j+97);
+            }
         }
-        return sb.toString();
+
+        return answer;
     }
 }
