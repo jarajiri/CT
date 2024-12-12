91da32496b879f67d60d5d5cae5ca8dd37331180
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2024-12-13 01:43:03 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 95.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ Java/프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int count = 1;
-        int point = 0;
-        while (count < k) {
-            point += 2;
-            point %= numbers.length;
-            count++;
-        }
-        return numbers[point];        
+        return numbers[(k-1)*2%numbers.length];
     }
 }
