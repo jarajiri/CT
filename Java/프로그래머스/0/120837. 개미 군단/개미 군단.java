dd843ba0bdc35228c13cb69568785cbdbde269ec
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2024-12-12 20:37:22 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,24 +1,5 @@
 class Solution {
-    static int answer;
-
-    public static int solution(int hp) {
-        int remain = countJang(hp);
-        int normal = countBong(remain);
-        answer += normal;
-        return answer;
-    }
-
-    private static int countBong(int remain) {
-        answer += remain / 3;
-        return remain % 3;
-    }
-
-    private static int countJang(int hp) {
-        answer += hp / 5;
-        return hp % 5;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("solution(23) = " + solution(999));
+    public int solution(int hp) {
+        return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3);
     }
 }
