c9f1431194d4fcec12d9673fb94df053171a8411
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2024-12-30 15:13:54 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 90 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = array[0];
+        int maxIdx = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIdx = i;
+            }
+        }
+        answer[0] = max;
+        answer[1] = maxIdx;
+        return answer;
+    }
+}
