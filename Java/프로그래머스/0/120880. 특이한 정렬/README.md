8617809969a948abc37dd180f6a7ceb96f1605b0
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-03-03 16:44:17 +0900

    [level 0] Title: 특이한 정렬, Time: 0.14 ms, Memory: 92.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120880. 특이한 정렬/README.md
+++ Java/프로그래머스/0/120880. 특이한 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특이한 정렬 - 120880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120880?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120880) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.36 ms
+메모리: 92.6 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 15:37:43
+2025년 03월 03일 16:44:10
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ Java/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,22 +1,27 @@
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        for (int i = 0; i < numlist.length - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < numlist.length; j++) {
-                int leftDistance = Math.abs(numlist[minIdx] - n);
-                int rightDistance = Math.abs(numlist[j] - n);
-                if (leftDistance > rightDistance || 
-                    leftDistance == rightDistance && numlist[minIdx] < numlist[j]) {
-                    minIdx = j;
+        int N = numlist.length;
+
+        for (int i = 1; i < N; i++) {
+            int key = numlist[i];
+            int keyDist = Math.abs(key - n);
+            int j = i - 1;
+
+            // 정렬된 부분에서 삽입 위치 찾기
+            while (j >= 0) {
+                int prevDist = Math.abs(numlist[j] - n);
+
+                if (prevDist < keyDist ||
+                    (prevDist == keyDist && numlist[j] >= key)) {
+                    break; // 정렬 조건을 만족하면 종료
                 }
+
+                numlist[j + 1] = numlist[j]; // 한 칸 뒤로 이동
+                j--;
             }
 
-            if (i != minIdx) {
-                int temp = numlist[i];
-                numlist[i] = numlist[minIdx];
-                numlist[minIdx] = temp;
-            }
+            numlist[j + 1] = key; // 적절한 위치에 삽입
         }
         return numlist;
     }
