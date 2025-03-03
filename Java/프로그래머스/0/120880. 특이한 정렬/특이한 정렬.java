07fde2a54ceb33d61400f1c8d6ccbdba8ba2ebcf
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-03-03 15:37:47 +0900

    [level 0] Title: 특이한 정렬, Time: 0.36 ms, Memory: 74.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ Java/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,24 +1,23 @@
 
-
 class Solution {
     public int[] solution(int[] numlist, int n) {
         for (int i = 0; i < numlist.length - 1; i++) {
+            int minIdx = i;
             for (int j = i + 1; j < numlist.length; j++) {
-                int leftDistance = Math.abs(numlist[i] - n);
+                int leftDistance = Math.abs(numlist[minIdx] - n);
                 int rightDistance = Math.abs(numlist[j] - n);
-                if (leftDistance > rightDistance) {
-                    int temp = numlist[i];
-                    numlist[i] = numlist[j];
-                    numlist[j] = temp;
-                } else if (leftDistance == rightDistance) {
-                    if (numlist[i] < numlist[j]) {
-                        int temp = numlist[i];
-                        numlist[i] = numlist[j];
-                        numlist[j] = temp;
-                    }
+                if (leftDistance > rightDistance || 
+                    leftDistance == rightDistance && numlist[minIdx] < numlist[j]) {
+                    minIdx = j;
                 }
             }
+
+            if (i != minIdx) {
+                int temp = numlist[i];
+                numlist[i] = numlist[minIdx];
+                numlist[minIdx] = temp;
+            }
         }
-        return numlist;     
+        return numlist;
     }
 }
