512baf02a3311aea1264d5da5f48bd75a4d4f88d
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-03-17 19:41:45 +0900

    [level 0] Title: 옹알이 (1), Time: 6.26 ms, Memory: 74.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,22 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] babbling){
-        String[] can = {"aya", "ye", "woo", "ma"};
-        int[] counts = new int[babbling.length];
-        for (int i = 0; i < babbling.length; i++) {
-            int count = 0;
-            String replace = "";
-            for (int j = 0; j < 4; j++) {
-                if (babbling[i].contains(can[j])) {
-                    babbling[i] = babbling[i].replace(can[j], " ");
-                    count++;
-                }
-            }
-            if (babbling[i].isBlank()) {
-                counts[i] = count;
-            }
+        int N = babbling.length;
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            babbling[i] = babbling[i].replace("aya","-");
+            babbling[i] = babbling[i].replace("ye","-");
+            babbling[i] = babbling[i].replace("woo","-");
+            babbling[i] = babbling[i].replace("ma","-");
+            babbling[i] = babbling[i].replace("-","");
+            
+            if(babbling[i].isEmpty()) count++;            
         }
-        return (int) Arrays.stream(counts).filter(i->i>0).count();
+        return count;
     }
 }
