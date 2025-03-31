59a4aa6dec1e3a6649bc12c5b831fe81c4ee5481
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-03-31 19:13:51 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int chicken) {
-        int service = 0;
-        int coupon = chicken;
-
-        while (coupon >= 10) {
-            int newChicken = coupon / 10;
-            service += newChicken;
-            coupon = newChicken + (coupon % 10); 
+        int answer = chicken/9;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
 
-        return service;
+
+        return answer;
     }
 }
