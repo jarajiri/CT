c1c6830c46e6658236ff2e263f9eebb136673e1c
Seongmin, Kim <140472588+jarajiri@users.noreply.github.com>
2025-04-01 09:04:37 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120884. 치킨 쿠폰/README.md
+++ Java/프로그래머스/0/120884. 치킨 쿠폰/README.md
@@ -1,10 +1,10 @@
 # [level 0] 치킨 쿠폰 - 120884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884) 
 
 ### 성능 요약
 
-메모리: 87.3 MB, 시간: 0.02 ms
+메모리: 76.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 31일 19:13:48
+2025년 04월 01일 09:04:34
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ Java/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int chicken) {
-        int answer = chicken/9;
-        if(chicken>1&&chicken%9==0){
-            answer--;
+        int service = 0;
+        int coupon = chicken;
+        while(coupon >= 10){
+            int newChicken = coupon / 10;
+            coupon = newChicken + (coupon % 10);
+            service += newChicken;
         }
-
-
-        return answer;
+        return service;
     }
 }
